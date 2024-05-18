162bd2e7f5f0b1f4324b25f551762e1e558157ff
DThemse <de.themse@gmail.com>
2024-05-18 04:43:30 +0300

    refactor: styles and jsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { LayoutProps } from '@/types/app';
-import './globals.css';
+import '@/styles/main.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
